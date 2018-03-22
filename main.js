b5090e49ec425ebf40211fcbc4595302d4553e8d
Michael <michael.m.scarr@gmail.com>
2018-03-22 11:39:28 -0500

    Check it

---
+++ main.js
@@ -0,0 +1,11 @@
+$("#searchButton").on("click", function () {
+    if ($("#searchOptions").val()=="tv") {
+
+    }
+    if ($("#searchOptions").val()=="movies") {
+
+    }
+    if ($("#searchOptions").val()=="videogames") {
+
+    }
+})
